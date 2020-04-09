8198137f99389d37e115668a789b799c8879dc0c
Raúl Contreras <raulcontrerasrubio@gmail.com>
2020-04-09 13:40:12 +0200

    Added secrets file

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+exports.redisHost = process.env.REDIS_HOST;
+exports.redisPort = process.env.REDIS_PORT;
+exports.dbHost = process.env.DB_HOST;
+exports.dbDatabase = process.env.DB_DATABASE;
+exports.dbPort = process.env.DB_PORT;
+exports.dbUser = process.env.DB_USER;
+exports.dbPassword = process.env.DB_PASSWORD;
+
+module.exports = exports;
